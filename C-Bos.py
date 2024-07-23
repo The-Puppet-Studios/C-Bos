7d29d554a28a47de94d735b8f51ffe87ca69f9b7
Puppet <122136792+Thepuppetqueen57@users.noreply.github.com>
2024-07-23 13:36:18 -0700

    Changed version number
    Changed my mind on it being 2.1.4 it is a pretty big update so its 2.2.0

--- C-Bos.py
+++ C-Bos.py
@@ -9,7 +9,7 @@ cboslib.settitle("C-Bos")
 
 cboslib.cprint("Booting cbos the crappy os...")
 time.sleep(0.5)
-version = 2.14
+version = 2.20
 cboslib.cprint("Checking for updates...")
 cboslib.versioncheck(version)
 time.sleep(0.3)
